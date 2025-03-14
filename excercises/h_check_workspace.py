dfe0c81843c24f30e39adb64b7cecfa809f62199
danielfhnw <daniel.jakober@fhnw.ch>
2025-03-14 17:48:55 +0100

    added sol h

--- excercises/h_check_workspace.py
+++ excercises/h_check_workspace.py
@@ -1,3 +1,10 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="get x and y")
+    parser.add_argument("x", type=float, help="x_soll")
+    parser.add_argument("y", type=float, help="y_soll")
+    args = parser.parse_args()
 # ------------------------------------------------------------------------------------------------
 
 # TODO change code only inside the check_ functions
@@ -17,3 +24,6 @@ def check_elbow_right(x, y):
 
 
 # ------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    print(f"Coordinates x: {args.x}, y: {args.y} are in the workspace with elbow left configuration: {check_elbow_left(args.x, args.y)}")
+    print(f"Coordinates x: {args.x}, y: {args.y} are in the workspace with elbow right configuration: {check_elbow_right(args.x, args.y)}")
