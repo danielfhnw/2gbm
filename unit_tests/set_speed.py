0a3077fd5efef0ac22061be8dd95afce654e1221
danielfhnw <daniel.jakober@fhnw.ch>
2025-03-21 10:10:38 +0100

    err corr

--- unit_tests/set_speed.py
+++ unit_tests/set_speed.py
@@ -12,57 +12,57 @@ if __name__ == "__main__":
     parser.add_argument("speed1", type=int, help="speed1")
     args = parser.parse_args()
 
-load_dotenv()
+    load_dotenv()
 
-com_port_motor = os.getenv("COM_PORT_MOTOR")
-portHandler = PortHandler(com_port_motor)
-packetHandler = sts(portHandler)
-    
-# Open port
-if portHandler.openPort():
-    print("Succeeded to open the port")
-else:
-    print("Failed to open the port")
-    quit()
+    com_port_motor = os.getenv("COM_PORT_MOTOR")
+    portHandler = PortHandler(com_port_motor)
+    packetHandler = sts(portHandler)
+        
+    # Open port
+    if portHandler.openPort():
+        print("Succeeded to open the port")
+    else:
+        print("Failed to open the port")
+        quit()
 
-# Set port baudrate
-if portHandler.setBaudRate(1000000):
-    print("Succeeded to change the baudrate")
-else:
-    print("Failed to change the baudrate")
-    quit()
+    # Set port baudrate
+    if portHandler.setBaudRate(1000000):
+        print("Succeeded to change the baudrate")
+    else:
+        print("Failed to change the baudrate")
+        quit()
 
-sts_comm_result, sts_error = packetHandler.WheelModeMode(1)
-if sts_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(sts_comm_result))
-elif sts_error != 0:
-    print("%s" % packetHandler.getRxPacketError(sts_error))
+    sts_comm_result, sts_error = packetHandler.WheelModeMode(1)
+    if sts_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(sts_comm_result))
+    elif sts_error != 0:
+        print("%s" % packetHandler.getRxPacketError(sts_error))
 
-try:
-    while True:
-        # Read STServo present position
-        servo_position, servo_speed, sts_comm_result, sts_error = packetHandler.ReadPosSpeed(1)
-        if sts_comm_result != COMM_SUCCESS:
-            print(packetHandler.getTxRxResult(sts_comm_result))
-        if sts_error != 0:
-            print(packetHandler.getRxPacketError(sts_error))
+    try:
+        while True:
+            # Read STServo present position
+            servo_position, servo_speed, sts_comm_result, sts_error = packetHandler.ReadPosSpeed(1)
+            if sts_comm_result != COMM_SUCCESS:
+                print(packetHandler.getTxRxResult(sts_comm_result))
+            if sts_error != 0:
+                print(packetHandler.getRxPacketError(sts_error))
 
-        # Write STServo goal position/moving speed/moving acc
-        sts_comm_result, sts_error = packetHandler.WriteSpec(1, args.speed1, 0)
+            # Write STServo goal position/moving speed/moving acc
+            sts_comm_result, sts_error = packetHandler.WriteSpec(1, args.speed1, 0)
+            if sts_comm_result != COMM_SUCCESS:
+                print("%s" % packetHandler.getTxRxResult(sts_comm_result))
+            if sts_error != 0:
+                print("%s" % packetHandler.getRxPacketError(sts_error))
+
+            print(f"Position: {servo_position}, Geschwindigkeit: {servo_speed}")
+
+    except KeyboardInterrupt:
+        print("Program stopped")
+
+    finally:
+        sts_comm_result, sts_error = packetHandler.WriteSpec(STS_ID, 0, 0)
         if sts_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler.getTxRxResult(sts_comm_result))
         if sts_error != 0:
             print("%s" % packetHandler.getRxPacketError(sts_error))
-
-        print(f"Position: {servo_position}, Geschwindigkeit: {servo_speed}")
-
-except KeyboardInterrupt:
-    print("Program stopped")
-
-finally:
-    sts_comm_result, sts_error = packetHandler.WriteSpec(STS_ID, 0, 0)
-    if sts_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(sts_comm_result))
-    if sts_error != 0:
-        print("%s" % packetHandler.getRxPacketError(sts_error))
-    portHandler.closePort() # Close port
+        portHandler.closePort() # Close port
