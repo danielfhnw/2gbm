1b661d6dc4ceb751ba12451828a01cae66661fde
danielfhnw <daniel.jakober@fhnw.ch>
2026-03-11 09:49:18 +0100

    added ex g

--- oop/g_invKin.py
+++ oop/g_invKin.py
@@ -3,9 +3,9 @@ from Robot import Robot
 if __name__ == "__main__":
     robot = Robot()
     try:
+        x, y = map(float, input("set tcp position (x y): ").split())
+        robot.set_tcp_position([x, y, 0, 1])
         while True:
-            x, y = map(float, input("set tcp position (x y): ").split())
-            robot.set_tcp_position([x, y, 0, 1])
             robot.print_tcp_position()
     except KeyboardInterrupt:
         print()
