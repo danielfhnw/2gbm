14c743030196d1c6ebe87d9678f604e865c61869
danielfhnw <daniel.jakober@fhnw.ch>
2026-03-18 09:42:20 +0100

    err corr

--- solutions/Robot.py
+++ solutions/Robot.py
@@ -37,7 +37,7 @@ class Robot:
         self.motor_1.shutdown()
         self.motor_2.shutdown()
         self.port_handler.closePort()
-        self.print("Robot shutdown")
+        print("Robot shutdown")
   
     def get_motor_positions(self, raw=False):
         if raw:
